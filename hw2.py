a62f32267ec6d92952247878fae524937ec6351f
Peggggy <smallereyes@gmail.com>
2018-07-02 23:44:28 +0800

    change

--- hw2.py
+++ hw2.py
@@ -12,14 +12,21 @@
 #     print(n1/n2)
 
 
-n=int(input("輸入一個正整數"))
-answer = 1
-while answer < n:
-    if answer*answer == n:
-        print(answer)
-        break
-    answer+=1
-if answer == n:
-    print("no1")
+# n=int(input("輸入一個正整數"))
+# answer = 1
+# while answer < n:
+#     if answer*answer == n:
+#         print(answer)
+#         break
+#     answer+=1
+# if answer == n:
+#     print("no1")
 
 # for n in range(1,10):
+
+x = 1
+while x < 10:
+    for y in range(1,10):
+        print(x,"*", y,"=" , x*y)
+    x+=1
+
